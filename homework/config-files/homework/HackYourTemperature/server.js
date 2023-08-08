23d1bf9c73c376e168235a3f7ca33e8ea30f8fe7
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-08-08 17:18:58 +0200

    create server for HackYourTemperature I

--- homework/config-files/homework/HackYourTemperature/server.js
+++ homework/config-files/homework/HackYourTemperature/server.js
@@ -1,2 +1,21 @@
-
+import express from 'express';
 import fetch from 'node-fetch';
+
+const app = express()
+const port = 3000
+app.use(express.json())
+//  Create a Send route
+app.get("/", (req, res) => {
+  res.send("hello from backend to frontend!!");
+});
+
+// Create a POST route
+app.post('/weather', (req, res) => {
+const { cityName } = req.body
+res.send(`The exact words you submitted ${cityName}`)
+})
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server running on port${port}`)
+})
